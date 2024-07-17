314d272a89be85ecdbee83625f39657eca263151
cs_lee <47213853+2044smile@users.noreply.github.com>
2024-07-18 00:38:22 +0900

    Feat: make a models and serializers

---
+++ app/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from app.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+
+
+class SnippetSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    code = serializers.CharField(style={'base_template': 'textarea.html'})
+    linenos = serializers.BooleanField(required=False)  # default 가 명시적으로 설정되지 않으면 기본 값은 None 이 됩니다.
+    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+
+    def create(self, validated_data):
+        """
+        검증한 데이터로 새 `Snippet` 인스턴스를 생성하여 리턴합니다.
+        """
+        return Snippet.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        """
+        검증한 데이터로 기존 `Snippet` 인스턴스를 업데이트한 후 리턴합니다.
+        """
+        instance.title = validated_data.get('title', instance.title)
+        instance.code = validated_data.get('code', instance.code)
+        instance.linenos = validated_data.get('linenos', instance.linenos)
+        instance.language = validated_data.get('language', instance.language)
+        instance.style = validated_data.get('style', instance.style)
+        instance.save()
+        return instance
