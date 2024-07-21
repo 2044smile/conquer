bcb990d12b87826b55683c40f1cd483ddd87c10b
cs_lee <47213853+2044smile@users.noreply.github.com>
2024-07-21 21:14:09 +0900

    Feat: generics

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from app import views
 
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
+    # path('snippets/', views.snippet_list),
+    # path('snippets/<int:pk>/', views.snippet_detail),
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)  # views 에서 사용한 format=None과 format_suffix_patterns() 함수는 URL 패턴에 형식 접미사를 추가하는 데 사용됩니다.
